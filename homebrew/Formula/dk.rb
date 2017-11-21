a6165556698898d6f7d04e883d4a7de09f714cd8
arnolda <>
2017-11-21 21:52:59 +1100

    bumping version

--- homebrew/Formula/dk.rb
+++ homebrew/Formula/dk.rb
@@ -1,7 +1,7 @@
 class Dk < Formula
   desc ""
   homepage "https://github.com/aa-neg/deploy-kite-cli"
-  url "https://github.com/aa-neg/deploy-kite-cli/blob/master/homebrew/Release/dk-0.0.1.tar.gz?raw=true"
+  url "https://github.com/aa-neg/deploy-kite-cli/blob/master/homebrew/Release/dk-0.0.2.tar.gz?raw=true"
   sha256 "97e4d2c7749b64422961a254a22eb44d86ebcd62048eb1cd9e3aae0e0868fe52"
 
   def install
