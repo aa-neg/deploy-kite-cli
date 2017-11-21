cdbbe8ae03887f0374d447cbe34a84cc70c6c9ed
arnolda <>
2017-11-21 21:54:02 +1100

    updating checksum

--- homebrew/Formula/dk.rb
+++ homebrew/Formula/dk.rb
@@ -2,7 +2,7 @@ class Dk < Formula
   desc ""
   homepage "https://github.com/aa-neg/deploy-kite-cli"
   url "https://github.com/aa-neg/deploy-kite-cli/blob/master/homebrew/Release/dk-0.0.2.tar.gz?raw=true"
-  sha256 "97e4d2c7749b64422961a254a22eb44d86ebcd62048eb1cd9e3aae0e0868fe52"
+  sha256 "61cc8ce6613591899a91536ad466c3e2ff9265d8164eafba03562deb21336511"
 
   def install
     bin.install "dk"
