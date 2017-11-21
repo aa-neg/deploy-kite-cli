d239a76427bf107fbe584d0cb1a4ac57c0ff6a77
arnolda <>
2017-11-21 19:58:17 +1100

    Adding brew formula file

---
+++ homebrew/Formula/dk.rb
@@ -0,0 +1,11 @@
+class Dk < Formula
+  desc ""
+  homepage "https://github.com/aa-neg/deploy-kite-cli"
+  url "https://github.com/aa-neg/deploy-kite-cli/blob/master/homebrew/Release/dk-0.0.1.tar.gz?raw=true"
+  sha256 "97e4d2c7749b64422961a254a22eb44d86ebcd62048eb1cd9e3aae0e0868fe52"
+
+  def install
+    bin.install "dk"
+    etc.install "config.json"
+  end
+end
